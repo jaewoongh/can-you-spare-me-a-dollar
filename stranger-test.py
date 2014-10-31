64ed6567b066fd7856ac43ae37c71c75623505fa
Jaewoong Hwang <Jaewoong@172-16-242-53.DYNAPOOL.NYU.EDU>
2014-10-31 18:19:38 -0400

    deleted test file

--- stranger-test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import random
-import requests
-
-user_id = random.randint(1300000000000000000, 1600000000000000000)
-print user_id
-stranger = requests.get('https://api.venmo.com/v1/users/' + str(user_id) + '?access_token=KARtLK2YAdgaA9tKV6naPXw7hay8Xxnk').json()
-print stranger
